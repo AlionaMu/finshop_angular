6cbb85b48a37d1c5b50b237cdd8dd7a3148751a9
AlionaMu <alionazimaby@gmail.com>
2022-09-28 14:43:55 +0300

    feat: implement carousel slider for partners

--- src/app/promo/components/promo/promo.component.ts
+++ src/app/promo/components/promo/promo.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+const images = [
+  {path: '../../../assets/images/sber.png'},
+  {path: '../../../assets/images/finshop.png'},
+  {path: '../../../assets/images/dabrabyt.png'},
+  {path: '../../../assets/images/belagro.png'},
+  {path: '../../../assets/images/belveb.png'},
+  {path: '../../../assets/images/alfa.png'}
+]
+
 @Component({
   selector: 'app-promo',
   templateUrl: './promo.component.html',
   styleUrls: ['./promo.component.scss']
 })
 export class PromoComponent implements OnInit {
-
+  public images: any = images
   constructor() { }
 
   ngOnInit(): void {
--- src/app/promo/promo.module.ts
+++ src/app/promo/promo.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PromoComponent } from './components/promo/promo.component';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
 
 @NgModule({
   declarations: [
     PromoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    IvyCarouselModule
   ],
   exports: [
     PromoComponent
